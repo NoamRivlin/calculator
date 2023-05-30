f1b887367a50f4e31b8eb78ad3ed71936a932b05
Noam Rivlin <noam.rivlin15@gmail.com>
2023-05-30 20:05:49 +0300

    refactor to address edge cases
    problem: can't figure out a way to prevent more than one decimal in a number - 2.2.2

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,31 @@ function App() {
   const [input, setInput] = useState<string>("");
   // Array of digits and decimal point to be displayed as buttons
   // Includes both numbers and string representation of the decimal point (".")
-  const DIGITS: (number | string)[] = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+  const DIGITS: (number | string)[] = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "."];
   const OPERATORS: string[] = ["+", "-", "*", "/"];
 
+  // TODO prevent multiple decimel points - 2.2.2
   const handleDigitClick = (digit: number | string) => {
+    const lastCharacter = input.slice(-1);
+    // Ignore consecutive decimal points or decimal point at the beginning of input
+    if (
+      (digit === "." && input === "") ||
+      (digit === "." && lastCharacter === ".")
+    ) {
+      return;
+    }
+
     setInput(input + digit);
   };
 
   const handleOperatorClick = (operator: string) => {
-    // Preventing consecutive operators that could break the calculation
+    // ignores division or multiplication operators if input is empty
+    if (input === "" && (operator === "/" || operator === "*")) return;
+
+    // Preventing consecutive operators that could break the calculation, eval doesn't support ** or //
     const lastCharacter = input.slice(-1);
     const isLastCharacterOperator = OPERATORS.includes(lastCharacter);
+    // if the last character is not an operator or if the last character is an operator and the new operator is - or +
     if (
       !isLastCharacterOperator ||
       (isLastCharacterOperator && (operator === "-" || operator === "+"))
@@ -28,25 +42,72 @@ function App() {
   };
 
   const handleClearEntry = () => {
+    // If the input is Infinity, -Infinity, or NaN, clear the input
     if (input === "Infinity" || input === "-Infinity" || input === "NaN") {
       setInput("");
     } else {
+      // Remove the last character from the input
       setInput(input.slice(0, -1));
     }
   };
 
   const handleCalculate = () => {
     try {
+      // Remove leading zeros from decimal numbers
+      // Convert each number (including decimals) in the input to its string representation without leading zeros
+      // For example, "01.23" becomes "1.23"
       let sanitizedInput = input.replace(/(\d+(\.\d+)?)/g, (match) =>
         parseFloat(match).toString()
-      ); // Remove leading zeros from decimal numbers
-      sanitizedInput = sanitizedInput.replace("--", "+"); // Replace consecutive "--" with "+"
+      );
+      // Replace consecutive "--" with "+"
+      sanitizedInput = sanitizedInput.replace("--", "+");
+
       const calculatedResult = eval(sanitizedInput);
       setInput(calculatedResult.toString());
-    } catch (error) {
-      console.log(error);
+    } catch (error: any) {
+      setInput(error);
     }
   };
+
+  // managed to ignore decimals after decimals - 2.2.2 + 2 will be calculated as 2.2+2
+  // BUT fails on long calculations...
+  // const handleCalculate = () => {
+  //   // calculate ignores decimals after decimals - 2.2.2 + 2 will be calculated as 2.2+2
+  //   try {
+  //     // Split the input into separate numbers based on operators (+, -, *, /)
+  //     const numbers = input.split(/[-+*/]/);
+
+  //     // Convert each number in the input to its string representation without leading zeros
+  //     const parsedNumbers = numbers.map((num) => {
+  //       if (num.includes(".")) {
+  //         // If the number contains a decimal point, remove leading zeros
+  //         return parseFloat(num).toString();
+  //       } else {
+  //         // If the number is an integer, remove leading zeros
+  //         return parseFloat(num).toString().replace(/^0+/, "");
+  //       }
+  //     });
+
+  //     // Reconstruct the expression by combining the parsed numbers and operators
+  //     let expression = "";
+  //     for (let i = 0; i < parsedNumbers.length; i++) {
+  //       expression += parsedNumbers[i];
+  //       if (i < numbers.length - 1) {
+  //         // Append the operator based on the length of the current number
+  //         expression += input.charAt(numbers[i].length);
+  //       }
+  //     }
+
+  //     // Evaluate the expression using eval and calculate the result
+  //     const calculatedResult = eval(expression);
+
+  //     // Set the input to the calculated result
+  //     setInput(calculatedResult.toString());
+  //   } catch (error: any) {
+  //     // Set the input to the error message if an error occurs during calculation
+  //     setInput(error);
+  //   }
+  // };
 
   return (
     <>
@@ -82,9 +143,6 @@ function App() {
                 {digit}
               </Button>
             ))}
-            <Button variant="contained" onClick={() => handleDigitClick(".")}>
-              .
-            </Button>
             <Button variant="contained" onClick={() => handleCalculate()}>
               =
             </Button>
